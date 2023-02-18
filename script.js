cf2856d756b3c20abdafb9c5482cc42f32df4cff
Kittencode91 <62710872+Kittencode91@users.noreply.github.com>
2023-02-18 14:59:55 +0000

    created arrays for messages and random numgen

---
+++ script.js
@@ -0,0 +1,14 @@
+function randomNum(num) {
+  return  Math.floor(Math.random() * 12)
+}
+
+console.log(randomNum());
+
+//const todaysDate = newDate.now();
+const message = ['You will meet a new friend soon', 'Be patient with your loved ones', 'Donate to a good cause', 'Be confident in your skills', 'You will have good luck', 'Money will come into your possession', 'Try something new'];
+const zodiac = ['Aquarius: 20th January - 18th February','Pieces: 19th February  - 20th March','Aries: 21st March - 19th April','Taurus: 20th April - 20th May','Gemini: 21st May - 20th June', 'Cancer: 21st June - 22nd July','Leo: 23rd July - 22nd August','Virgo: 23rd August - 22nd September','Libra: 23rd September - 22nd October','Scorpio: 23rd October - 21st November','Sagittarius: 22nd November - 21st December', 'Capricorn: 22nd December - 19th January' ];
+const horoscope = [];
+
+switch (prop) {
+    case 0: 
+}
